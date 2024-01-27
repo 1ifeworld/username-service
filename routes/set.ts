bf5332b90b5d4b4d11259b55317bafcd02aa4480
jawndiego <jawndiego@gmail.com>
2024-01-27 11:05:02 -0500

    debug header issue in username

--- routes/set.ts
+++ routes/set.ts
@@ -6,8 +6,6 @@ import { useCORS } from 'nitro-cors'
 import { HTTPMethod } from './getIdByOwner'
 
 export default defineEventHandler(async (event) => {
-  console.log('setname')
-
   // Define CORS options
   const corsOptions = {
     methods: ['GET', 'POST', 'OPTIONS'] as HTTPMethod[],
@@ -22,70 +20,71 @@ export default defineEventHandler(async (event) => {
   // Apply CORS to the request
   useCORS(event, corsOptions)
 
-  // Handle preflight (OPTIONS) request
   if (event.node.req.method === 'OPTIONS') {
-    return { statusCode: 204 } // End the response for OPTIONS request
-  }
-  // Ensure this is a POST request
-  if (event.node.req.method !== 'POST') {
-    return { error: 'Method not allowed', statusCode: 405 }
-  }
-  let body
-  try {
-    body = await readBody(event)
-  } catch (error) {
-    console.error('Error parsing request body:', error)
-    return { success: false, error: 'Invalid request', statusCode: 400 }
-  }
+  } else if (event.node.req.method !== 'POST') {
+    return createError({ statusCode: 405, statusMessage: 'Method not allowed' })
+  } else {
+    try {
+      let body
+      try {
+        body = await readBody(event)
+      } catch (error) {
+        console.error('Error parsing request body:', error)
+        return { success: false, error: 'Invalid request', statusCode: 400 }
+      }
 
-  // Extract data from the body for Zod validation
+      // Extract data from the body for Zod validation
 
-console.log("body", body)
+      console.log('body', body)
 
-// Validate the data with Zod
-const parseResult = ZodName.safeParse( body)
-console.error('Invalid input', parseResult.success);
-if (!parseResult.success) {
-  console.error('Invalid input')
-  return { success: false, error: 'Invalid input', statusCode: 400 }
-}
+      // Validate the data with Zod
+      const parseResult = ZodName.safeParse(body)
+      console.error('Invalid input', parseResult.success)
+      if (!parseResult.success) {
+        console.error('Invalid input')
+        return { success: false, error: 'Invalid input', statusCode: 400 }
+      }
 
-  // Validate signature
-  try {
-    const messageToVerify = JSON.stringify(parseResult.data)
+      // Validate signature
+      try {
+        const messageToVerify = JSON.stringify(parseResult.data)
 
-    const isValidSignature = verifyMessage({
-      address: parseResult.data.owner as Hex,
-      signature: parseResult.data.signature as Hex,
-      message: messageToVerify,
-    })
+        const isValidSignature = verifyMessage({
+          address: parseResult.data.owner as Hex,
+          signature: parseResult.data.signature as Hex,
+          message: messageToVerify,
+        })
 
-    if (!isValidSignature) {
-      throw new Error('Invalid signature')
+        if (!isValidSignature) {
+          throw new Error('Invalid signature')
+        }
+      } catch (err) {
+        console.error(err)
+        const response = { success: false, error: 'Invalid signature' }
+        return Response.json(response, { status: 401 })
+      }
+
+      // Check if the name is already taken
+      const existingName = await get(parseResult.data.name)
+      if (existingName && existingName.owner !== parseResult.data.owner) {
+        const response = { success: false, error: 'Name already taken' }
+        return Response.json(response, { status: 409 })
+      }
+
+      // Save the name
+      try {
+        await set(parseResult.data)
+        const response = { success: true }
+        return Response.json(response, { status: 201 })
+      } catch (err) {
+        console.error('Error caught in setName:', err)
+        const errorMessage =
+          err instanceof Error ? err.message : 'An unexpected error occurred'
+        const response = { success: false, error: errorMessage }
+        return Response.json(response, { status: 500 })
+      }
+    } catch (e) {
+      console.error('Error with Route', e)
     }
-  } catch (err) {
-    console.error(err)
-    const response = { success: false, error: 'Invalid signature' }
-    return Response.json(response, { status: 401 })
   }
-
-  // Check if the name is already taken
-  const existingName = await get(parseResult.data.name)
-  if (existingName && existingName.owner !== parseResult.data.owner) {
-    const response = { success: false, error: 'Name already taken' }
-    return Response.json(response, { status: 409 })
-  }
-
-  // Save the name
-  try {
-    await set(parseResult.data)
-    const response = { success: true }
-    return Response.json(response, { status: 201 })
-  } catch (err) {
-    console.error('Error caught in setName:', err)
-    const errorMessage =
-      err instanceof Error ? err.message : 'An unexpected error occurred'
-    const response = { success: false, error: errorMessage }
-    return Response.json(response, { status: 500 })
-  }}
-)
+})
