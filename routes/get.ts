46986b07d03931df2a13dcd0c9e7f4bf1b8ae3d9
jawndiego <jawndiego@gmail.com>
2024-02-01 12:37:56 -0500

    get refactor to read body

--- routes/get.ts
+++ routes/get.ts
@@ -1,68 +1,59 @@
-import zod from 'zod'
-import { get } from '../utils/functions/get'
-import { HTTPMethod } from './getIdByOwner'
-import { useCORS } from 'nitro-cors'
+import zod from "zod";
+import { get } from "../utils/functions/get";
+import { HTTPMethod } from "./getIdByOwner";
+import { useCORS } from "nitro-cors";
 
 export default defineEventHandler(async (event) => {
+  console.log("ROUTE HIT")
   // Define CORS options
   const corsOptions = {
-    methods: ['GET', 'POST', 'OPTIONS'] as HTTPMethod[],
+    methods: ["GET", "POST", "OPTIONS"] as HTTPMethod[],
     allowHeaders: [
-      'Authorization',
-      'Content-Type',
-      'Access-Control-Allow-Origin',
+      "Authorization",
+      "Content-Type",
+      "Access-Control-Allow-Origin",
     ],
     preflight: { statusCode: 204 },
-  }
+  };
 
   // Apply CORS to the request
-  useCORS(event, corsOptions)
+  useCORS(event, corsOptions);
 
-  if (event.node.req.method === 'OPTIONS') {
-  } else if (event.node.req.method !== 'POST') {
-    return createError({ statusCode: 405, statusMessage: 'Method not allowed' })
+  if (event.node.req.method === "OPTIONS") {
+  } else if (event.node.req.method !== "POST") {
+    return createError({
+      statusCode: 405,
+      statusMessage: "Method not allowed",
+    });
   } else {
     try {
-      // Manually parsing the request body
-      const bodyPromise = new Promise((resolve, reject) => {
-        let body = ''
-        event.node.req.on('data', (chunk) => {
-          body += chunk.toString()
-        })
-        event.node.req.on('end', () => {
-          try {
-            resolve(JSON.parse(body))
-          } catch (e) {
-            reject(e)
-          }
-        })
-      })
-
-      let body
-      try {
-        body = await bodyPromise
-      } catch (error) {
-        console.error('Error parsing request body:', error)
-        return Response.json({ error: 'Invalid input' }, { status: 400 })
-      }
+      console.log("PARSING BODY")
+      // Using readBody to parse the request body
+      const body = await readBody(event)
+      console.log("BODY", body)
 
       const schema = zod.object({
         username: zod
           .string()
-          .regex(/^[a-z0-9-.]+$/, 'Invalid username format'),
-      })
+          .regex(/^[a-z0-9-.]+$/, "Invalid username format"),
+      });
 
       const safeParse = schema.safeParse(body)
 
+      console.log("SAFE PARSE", safeParse)
+
       if (!safeParse.success) {
-        const response = { error: 'Invalid input' }
+        const response = { error: "Invalid input" };
         return Response.json(response, { status: 400 })
       }
 
       const { username } = safeParse.data
 
+      console.log("USERNAME", username)
+
       try {
         const nameData = await get(username)
+        console.log({nameData})
 
         if (nameData === null) {
           return Response.json({ exists: false }, { status: 404 })
@@ -70,14 +61,14 @@ export default defineEventHandler(async (event) => {
 
         return Response.json({ exists: true }, { status: 200 })
       } catch (error) {
-        console.error('Error fetching name data:', error)
+        console.error("Error fetching name data:", error)
         return Response.json(
-          { error: 'Internal Server Error' },
-          { status: 500 },
-        )
+          { error: "Internal Server Error" },
+          { status: 500 }
+        );
       }
     } catch (e) {
-      console.error('Error with Route', e)
+      console.error("Error with Route", e)
     }
   }
 })
