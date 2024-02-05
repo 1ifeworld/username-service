f5cd8207fdcb7ba8f98cf86bb2647e34fca7235f
jawndiego <jawndiego@gmail.com>
2024-02-05 00:05:22 -0500

    contracts

--- utils/constants/contracts.ts
+++ utils/constants/contracts.ts
@@ -19,8 +19,9 @@ export const addresses: AddressBook = {
   //   opGoerli: '0x6d8313bBac1C0596Ff115DE28c8E0E4d3328DbE3',
   // },
   postGateway: {
-    opGoerli: '0x339513226Afd92B309837Bad402c6D3ADDE9Ad24',
-    river_j5bpjduqfv: '0x1B692589017d4b2276227b52cD3A2a1796cb2b86',
+    // opGoerli: '0x339513226Afd92B309837Bad402c6D3ADDE9Ad24',
+    nova: '0x05aD6cA9C2b3F71a6B30A8C7d414C95E10EC0217'
+    // river_j5bpjduqfv: '0x1B692589017d4b2276227b52cD3A2a1796cb2b86',
     //   arbGoerli: "0x",
     //   anvil: "0x5FbDB2315678afecb367f032d93F642f64180aa3"
     // ... add more chains as needed
