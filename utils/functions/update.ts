8494d89ea67f670d5410caf456dd158aaeb22c19
jawndiego <jawndiego@gmail.com>
2024-04-09 09:07:33 -0500

    update route and function

--- utils/functions/update.ts
+++ utils/functions/update.ts
@@ -1,41 +1,40 @@
 import { createKysely } from '../createKysely'
 import { Name } from './../models'
 import { stringifyNameForDb } from '../utils'
+
 export async function updateNameAndArchive(nameData: Name) {
     const db = createKysely()
     const body = stringifyNameForDb(nameData)
 
     await db.transaction().execute(async (trx) => {
-      // Fetch the existing record by id
-      const existingRecord = await trx
-        .selectFrom('names')
-        .selectAll()
-        .where('id', '=', nameData.id)
-        .executeTakeFirst()
+        const existingRecord = await trx
+            .selectFrom('names')
+            .selectAll()
+            .where('id', '=', nameData.id)
+            .executeTakeFirst()
 
-      if (!existingRecord) {
-        throw new Error(`Record not found for ID: ${nameData.id}`)
-      }
-
-      // Check if the name is changing. If so, archive the current record.
-      if (existingRecord.name !== nameData.name) {
-        const historicalRecord = {
-          ...existingRecord,
+        if (!existingRecord) {
+            throw new Error(`Record not found for ID: ${nameData.id}`)
         }
-        await trx
-          .insertInto('historicalNames')
-          .values(historicalRecord)
-          .execute()
-      }
 
-      // Update the names record with the new data
-      await trx
-        .updateTable('names')
-        .set(body)
-        .where('id', '=', nameData.id)
-        .execute()
+        if (existingRecord.name !== nameData.name) {
+            const historicalRecord = {
+                ...existingRecord,
+            }
+            await trx
+                .insertInto('historicalNames')
+                .values(historicalRecord)
+                .execute()
+        }
+
+
+        await trx
+            .updateTable('names')
+            .set(body)
+            .where('id', '=', nameData.id)
+            .execute()
     }).catch((error) => {
-      console.error('Error in updating name and archiving:', error)
-      throw error
+        console.error('Error in updating name and archiving:', error)
+        throw error
     })
-  }
+}
