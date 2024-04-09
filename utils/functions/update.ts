3624f1dbd9f5f7f96e282a133af041089afd6b11
jawndiego <jawndiego@gmail.com>
2024-04-09 12:42:56 -0500

    append test

--- utils/functions/update.ts
+++ utils/functions/update.ts
@@ -38,18 +38,12 @@ export async function updateNameAndArchive(nameData: Name) {
 
             if (existingHistoricalRecord) {
                 if (existingHistoricalRecord) {
-                    const currentNameArray = Array.isArray(existingHistoricalRecord.name) ? existingHistoricalRecord.name : []
-                    const currentToArray = Array.isArray(existingHistoricalRecord.to) ? existingHistoricalRecord.to : []
-                    const currentOwnerArray = Array.isArray(existingHistoricalRecord.owner) ? existingHistoricalRecord.owner : []
-                    const currentSignatureArray = Array.isArray(existingHistoricalRecord.signature) ? existingHistoricalRecord.signature : []
-                    const currentTimestampArray = Array.isArray(existingHistoricalRecord.timestamp) ? existingHistoricalRecord.timestamp : []
 
-                    const updatedNameArray = [...currentNameArray, newEntries.name]
-                    const updatedToArray = [...currentToArray, newEntries.to]
-                    const updatedOwnerArray = [...currentOwnerArray, newEntries.owner]
-                    const updatedSignatureArray = [...currentSignatureArray, newEntries.signature]
-                    const updatedTimestampArray = [...currentTimestampArray, newEntries.timestamp]
-
+                    const updatedNameArray = [...(existingHistoricalRecord.name || []), newEntries.name]
+                    const updatedToArray =  [...(existingHistoricalRecord.to || []), newEntries.to]
+                    const updatedOwnerArray = [...(existingHistoricalRecord.owner || []), newEntries.owner]
+                    const updatedSignatureArray =  [...(existingHistoricalRecord.signature || []), newEntries.signature]
+                    const updatedTimestampArray =  [...(existingHistoricalRecord.timestamp || []), newEntries.timestamp]
                     await trx
                         .updateTable("historical_names")
                         .set({
