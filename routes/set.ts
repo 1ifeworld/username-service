c0f85a2de476fda4acd14add92384182163160f1
jawndiego <jawndiego@gmail.com>
2024-04-10 18:13:19 -0500

    new form

--- routes/set.ts
+++ routes/set.ts
@@ -188,176 +188,124 @@
 //   }
 // }
 
-
-import { ZodName } from '../utils/models'
-import { get } from '../utils/functions/get'
-import { set } from '../utils/functions/set'
-import { Hex, verifyMessage } from 'viem'
-import { useCORS } from 'nitro-cors'
-import { HTTPMethod } from './getIdByOwner'
-import { updateNameAndArchive } from '../utils/functions/update'
+import { ZodName } from '../utils/models';
+import { get } from '../utils/functions/get';
+import { set } from '../utils/functions/set';
+import { Hex, verifyMessage } from 'viem';
+import { useCORS } from 'nitro-cors';
+import { HTTPMethod } from './getIdByOwner';
+import { updateNameAndArchive } from '../utils/functions/update';
 
 export default defineEventHandler(async (event) => {
-  // Define CORS options
-  const corsOptions = {
-    methods: ['GET', 'POST', 'OPTIONS'] as HTTPMethod[],
-    allowHeaders: [
-      'Authorization',
-      'Content-Type',
-      'Access-Control-Allow-Origin',
-    ],
-    preflight: { statusCode: 204 },
-  }
+    const corsOptions = {
+        methods: ['GET', 'POST', 'OPTIONS'] as HTTPMethod[],
+        allowHeaders: ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin'],
+        preflight: { statusCode: 204 },
+    };
 
-  // Apply CORS to the request
-  useCORS(event, corsOptions)
+    useCORS(event, corsOptions);
 
-  if (event.node.req.method === 'OPTIONS') {
-  } else if (event.node.req.method !== 'POST') {
-    return createError({ statusCode: 405, statusMessage: 'Method not allowed' })
-  } else {
+    if (event.node.req.method === 'OPTIONS') {
+        // OPTIONS method is automatically handled by useCORS.
+        return;
+    } else if (event.node.req.method !== 'POST') {
+        return { success: false, statusCode: 405, error: 'Method not allowed' };
+    }
+
+    let body;
     try {
-      let body
-      try {
-        body = await readBody(event)
-      } catch (error) {
-        console.error('Error parsing request body:', error)
-        return { success: false, error: 'Invalid request', statusCode: 400 }
-      }
+        body = await readBody(event);
+    } catch (error) {
+        return { success: false, error: 'Error parsing request body', statusCode: 400 };
+    }
 
-      // Extract data from the body for Zod validation
+    const parseResult = ZodName.safeParse(body);
+    if (!parseResult.success) {
+        return { success: false, error: 'Invalid input', statusCode: 400 };
+    }
 
-      console.log('body', body)
+    const currentTimestampInSeconds = Math.floor(Date.now() / 1000);
+    const providedTimestamp = parseInt(parseResult.data.timestamp || '0');
+    if (providedTimestamp > currentTimestampInSeconds + 60) {
+        return { success: false, error: 'Invalid timestamp', statusCode: 400 };
+    }
 
-      // Validate the data with Zod
-      const parseResult = ZodName.safeParse(body)
-      if (!parseResult.success) {
-        console.error('Invalid input')
-        return { success: false, error: 'Invalid input', statusCode: 400 }
-      }
-      console.log("PARSE", parseResult.data.owner)
-      // time stamp check
-      const currentTimestamp = Math.floor(Date.now())
-      console.log(currentTimestamp)
-      const providedTimestamp = parseInt(parseResult.data.timestamp || '0')
-      if (providedTimestamp > currentTimestamp + 60) {
-        console.error('Invalid timestamp')
-        return { success: false, error: 'Invalid timestamp', statusCode: 400 }
-      }
-      let ownerId
-      try {
+    let ownerId;
+    try {
         ownerId = await publicClient.readContract({
-          address: addresses.idRegistry.optimism,
-          abi: idRegistryABI,
-          functionName: 'idOf',
-          args: [parseResult.data.owner as Hex],
-        })
-      } catch (error) {
-        console.error('Error fetching owner ID:', error)
-        return {
-          success: false,
-          error: 'Error fetching owner ID',
-          statusCode: 500,
-        }
-      }
+            address: addresses.idRegistry.optimism,
+            abi: idRegistryABI,
+            functionName: 'idOf',
+            args: [parseResult.data.owner as Hex],
+        });
+    } catch (error) {
+        return { success: false, error: 'Error fetching owner ID', statusCode: 500 };
+    }
 
-      console.log('Fetched owner ID:', ownerId)
-      console.log('ID to check for', parseResult.data.id)
+    if (ownerId.toString() !== parseResult.data.id) {
+        return { success: false, error: 'Not the owner of the ID', statusCode: 401 };
+    }
 
-      if (ownerId.toString() !== parseResult.data.id) {
-        return {
-          success: false,
-          error: 'Not the owner of the ID',
-          statusCode: 401,
-        }
-      }
-
-      console.log('TO OWNERSHIp')
-      let nameOwned
-      console.log("not json", parseResult.data.id)
-      console.log("json", JSON.stringify({ id: parseResult.data.id }))
-
-      try {
-        nameOwned = await fetch(
-          'https://username-service-production.up.railway.app/getUsernameById',
-          {
+    let nameOwned;
+    try {
+        const response = await fetch('https://username-service-production.up.railway.app/getUsernameById', {
             method: 'POST',
             body: JSON.stringify({ id: parseResult.data.id }),
             headers: { 'Content-Type': 'application/json' },
-          },
-        ).then((res) => res.json())
-      } catch (error) {
-        console.error('Error fetching username:', error)
-        console.log('NAME OWNED', nameOwned)
-        return {
-          success: false,
-          error: 'Unable to fetch username',
-          statusCode: 500,
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch username');
         }
-      }
-      // Validate signature
-      try {
-        const messageToVerify = JSON.stringify(parseResult.data)
+        nameOwned = await response.json();
+    } catch (error) {
+        return { success: false, error: 'Unable to fetch username', statusCode: 500 };
+    }
 
-        const isValidSignature = verifyMessage({
-          address: parseResult.data.owner as Hex,
-          signature: parseResult.data.signature as Hex,
-          message: messageToVerify,
-        })
+    const isValidSignature = verifyMessage({
+        address: parseResult.data.owner as Hex,
+        signature: parseResult.data.signature as Hex,
+        message: JSON.stringify(parseResult.data),
+    });
 
-        if (!isValidSignature) {
-          throw new Error('Invalid signature')
+    if (!isValidSignature) {
+        return { success: false, error: 'Invalid signature', statusCode: 401 };
+    }
+
+    const existingName = await get(parseResult.data.name);
+    if (existingName && existingName.owner !== parseResult.data.owner) {
+        return { success: false, error: 'Name already taken', statusCode: 409 };
+    }
+
+    let lastSetTimestamp;
+    try {
+        const response = await fetch('https://username-service-production.up.railway.app/getLastTimestamp', {
+            method: 'POST',
+            body: JSON.stringify({ id: parseResult.data.id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch last timestamp');
         }
-      } catch (err) {
-        console.error(err)
-        const response = { success: false, error: 'Invalid signature' }
-        return Response.json(response, { status: 401 })
-      }
+        const data = await response.json();
+        lastSetTimestamp = data.timestamp;
+    } catch (error) {
+        return { success: false, error: 'Unable to fetch last timestamp', statusCode: 500 };
+    }
 
-      // Check if the name is already taken
-      const existingName = await get(parseResult.data.name)
-      console.log({existingName})
+    const secondsIn14Days = 2419200/2; // 28 days in seconds
+    if (providedTimestamp - lastSetTimestamp < secondsIn14Days) {
+        return { success: false, error: 'Name change not allowed within 28 days', statusCode: 400 };
+    }
 
-      if (existingName && existingName.owner !== parseResult.data.owner) {
-        const response = { success: false, error: 'Name already taken' }
-        return Response.json(response, { status: 409 })
-      }
-
-
+    try {
         if (existingName) {
-          if (existingName && existingName.owner !== parseResult.data.owner) {
-            return { success: false, error: 'Not the owner of the name', statusCode: 401 }
-          }
-        let lastSetTimestamp
-          lastSetTimestamp = await fetch(
-            'https://username-service-production.up.railway.app/getLastTimestamp',
-            {
-              method: 'POST',
-              body: JSON.stringify({ id: parseResult.data.id }),
-              headers: { 'Content-Type': 'application/json' },
-            },
-          ).then((res) => res.json())
-
-          const secondsIn28Days = 2419200
-          if (providedTimestamp - lastSetTimestamp < secondsIn28Days) {
-            return {
-              success: false,
-              error: 'Name change not allowed within 28 days',
-              statusCode: 400,
-            }
-          } else if (existingName) {
-            await updateNameAndArchive(parseResult.data)
-            return { success: true, statusCode: 200 }
-
-          } else {
-
-        await set(parseResult.data)
-        const response = { success: true }
-        return Response.json(response, { status: 201 })
-      }
+            await updateNameAndArchive(parseResult.data);
+        } else {
+            await set(parseResult.data);
+        }
+        return { success: true, statusCode: existingName ? 200 : 201 };
+    } catch (error) {
+        const errorMessage = error instanceof Error ? error.message : 'An unexpected error occurred';
+        return { success: false, error: errorMessage, statusCode: 500 };
     }
-    } catch (e) {
-      console.error('Error with Route', e)
-    }
-  }
-})
+});
