3413a3166309ce36c3fda675de0958e1d32a0a54
jawndiego <jawndiego@gmail.com>
2024-04-11 08:50:05 -0500

    2 week enforcement

--- utils/functions/setOrupdate.ts
+++ utils/functions/setOrupdate.ts
@@ -1,3 +1,48 @@
+// // import { createKysely } from '../createKysely'
+// // import { Name } from '../models'
+// // import { stringifyNameForDb } from '../utils'
+
+// // export async function setOrUpdate(nameData: Name) {
+// //   const db = createKysely()
+// //   const body = stringifyNameForDb(nameData)
+
+// //   try {
+// //     console.log('in updateNameAndArchive')
+// //     await db.transaction().execute(async (trx) => {
+// //       const updateBody = { ...body }
+// //       delete updateBody.id
+
+// //       const existingNameRecord = await trx
+// //         .selectFrom('names')
+// //         .selectAll()
+// //         .where('id', '=', nameData.id)
+// //         .executeTakeFirst()
+
+// //       if (existingNameRecord && existingNameRecord.name !== nameData.name) {
+// //         console.log('update condition hit')
+
+// //         await trx.insertInto('changelog').values(body).execute()
+
+// //         await trx
+// //           .updateTable('names')
+// //           .set(updateBody)
+// //           .where('id', '=', nameData.id)
+// //           .execute()
+// //       } else if (!existingNameRecord) {
+// //         console.log('set condition hit')
+// //         await trx
+// //           .insertInto('names')
+// //           .values(body)
+// //           .onConflict((oc) => oc.column('name').doUpdateSet(body))
+// //           .execute()
+// //       }
+// //     })
+// //   } catch (error) {
+// //     console.error('Error in setting name:', error)
+// //     throw error
+// //   }
+// // }
+
 // import { createKysely } from '../createKysely'
 // import { Name } from '../models'
 // import { stringifyNameForDb } from '../utils'
@@ -5,6 +50,7 @@
 // export async function setOrUpdate(nameData: Name) {
 //   const db = createKysely()
 //   const body = stringifyNameForDb(nameData)
+//   const providedTimestamp = Date.now()
 
 //   try {
 //     console.log('in updateNameAndArchive')
@@ -17,6 +63,16 @@
 //         .selectAll()
 //         .where('id', '=', nameData.id)
 //         .executeTakeFirst()
+
+//       const lastUpdateTimestamp = existingNameRecord ? new Date(existingNameRecord.timestamp).getTime() : 0
+//       const secondsIn14Days = 1209600
+//       const currentTimestampInSeconds = Math.floor(Date.now() / 1000)
+//       const lastUpdateTimestampInSeconds = Math.floor(lastUpdateTimestamp / 1000)
+
+//       if (currentTimestampInSeconds - lastUpdateTimestampInSeconds < secondsIn14Days) {
+//         console.error('Name change not allowed within 14 days of last edit')
+//         throw new Error('Name change not allowed within 14 days of last edit')
+//       }
 
 //       if (existingNameRecord && existingNameRecord.name !== nameData.name) {
 //         console.log('update condition hit')
@@ -33,7 +89,6 @@
 //         await trx
 //           .insertInto('names')
 //           .values(body)
-//           .onConflict((oc) => oc.column('name').doUpdateSet(body))
 //           .execute()
 //       }
 //     })
@@ -43,6 +98,7 @@
 //   }
 // }
 
+
 import { createKysely } from '../createKysely'
 import { Name } from '../models'
 import { stringifyNameForDb } from '../utils'
@@ -50,10 +106,11 @@ import { stringifyNameForDb } from '../utils'
 export async function setOrUpdate(nameData: Name) {
   const db = createKysely()
   const body = stringifyNameForDb(nameData)
-  const providedTimestamp = Date.now()
+
+  const currentTimestampInSeconds = Math.floor(Date.now() / 1000)
 
   try {
-    console.log('in updateNameAndArchive')
+    console.log('in setOrUpdate')
     await db.transaction().execute(async (trx) => {
       const updateBody = { ...body }
       delete updateBody.id
@@ -64,28 +121,30 @@ export async function setOrUpdate(nameData: Name) {
         .where('id', '=', nameData.id)
         .executeTakeFirst()
 
-      const lastUpdateTimestamp = existingNameRecord ? new Date(existingNameRecord.timestamp).getTime() : 0
-      const secondsIn14Days = 1209600
-      const currentTimestampInSeconds = Math.floor(providedTimestamp / 1000)
-      const lastUpdateTimestampInSeconds = Math.floor(lastUpdateTimestamp / 1000)
+      if (existingNameRecord) {
+        const lastUpdateTimestampInSeconds = existingNameRecord.timestamp
+        const secondsIn14Days = 1209600
 
-      if (currentTimestampInSeconds - lastUpdateTimestampInSeconds < secondsIn14Days) {
-        console.error('Name change not allowed within 14 days of last edit')
-        throw new Error('Name change not allowed within 14 days of last edit')
-      }
+        if (currentTimestampInSeconds - lastUpdateTimestampInSeconds < secondsIn14Days) {
+          console.error('Name change not allowed within 14 days of last edit')
+          throw new Error('Name change not allowed within 14 days of last edit')
+        }
 
-      if (existingNameRecord && existingNameRecord.name !== nameData.name) {
-        console.log('update condition hit')
+        if (existingNameRecord.name !== nameData.name) {
+          console.log('Update condition hit')
 
-        await trx.insertInto('changelog').values(body).execute()
+        await trx.insertInto('changelog')
+        .values(body)
+        .execute()
 
-        await trx
+          await trx
           .updateTable('names')
           .set(updateBody)
           .where('id', '=', nameData.id)
           .execute()
-      } else if (!existingNameRecord) {
-        console.log('set condition hit')
+        }
+      } else {
+        console.log('Insert condition hit')
         await trx
           .insertInto('names')
           .values(body)
@@ -93,8 +152,7 @@ export async function setOrUpdate(nameData: Name) {
       }
     })
   } catch (error) {
-    console.error('Error in setting name:', error)
+    console.error('Error in setting or updating name:', error)
     throw error
   }
 }
-
